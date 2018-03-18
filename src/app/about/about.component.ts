39fbdf152e1c1c7a47385003974880e69aa380e5
rkemmy <rkemmy.rk69@gmail.com>
2018-03-18 22:02:57 +0300

    Link the logic files to the template files

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  quote = 'If you think education is expensive, try ignorance!'
 
   constructor() { }
 
