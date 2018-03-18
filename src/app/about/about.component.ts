35de56e2ad68f473215a227613da5c7e4e91cb7e
rkemmy <rkemmy.rk69@gmail.com>
2018-03-19 00:11:42 +0300

    Create a button that accepts new input by the user

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,14 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  quote = 'If you think education is expensive, try ignorance!'
+  quotes = ['If you think education is expensive, try ignorance!', 'Don’t fake it till you make it. Fake it till you become it.', 'All things are difficult before they are easy.']
 
   inputQuote = null;
-
   btnDisable = false;
+  Adding = false;
 
-  ChangeQuote(){
-    this.quote = this.inputQuote
+  CreateQuote(){
+    this.quotes.push(this.inputQuote)
+    this.inputQuote = null
+    this.Adding = false
+    
+  }
+
+  AddQuote(){
+    this.Adding = !this.Adding
   }
 
   constructor() { }
